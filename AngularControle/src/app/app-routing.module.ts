0dde8e88bd031796c26fb15bb7d01b3486ade74c
MohamedLM <laamarti.lm.93@gmail.com>
2020-05-05 13:58:02 +0000

    create page  contact.component.html install font-awesome config routing /contact

--- AngularControle/src/app/app-routing.module.ts
+++ AngularControle/src/app/app-routing.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'contact'
+  },
+  {
+    path: 'contact',
+    loadChildren: ()=> import('./contact/contact.module').then(m => m.ContactModule)
+  }
+];
 
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
